543bc92e7a8e0bc5928fbe9d3f103e2f141bdf1d
kalinin <kalinin>
2023-11-14 18:16:03 +0300

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,3 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: [
-    '@casino/base',
-  ],
-  // devtools: { enabled: true },
-  modules: ['@casino/api', '@nuxt/devtools'],
 })
